4775739f8e4a3557bafdf058f043200b7ac8e870
tbe00 <tbe00@192.168.0.167>
2022-09-27 20:41:13 +0200

    Oppgaver ferdig men ikke testet med JUnit

--- src/no/hvl/dat100/lab5/tabeller/Tabeller.java
+++ src/no/hvl/dat100/lab5/tabeller/Tabeller.java
@@ -2,63 +2,160 @@ package no.hvl.dat100.lab5.tabeller;
 
 public class Tabeller {
 
-	// a)
+	// a) FERDIG //////////////////////////////////////////////////////////////////////////
 	public static void skrivUt(int[] tabell) {
-
-		// TODO push test kommentar
-		throw new UnsupportedOperationException("skrivUt ikke implementert");
-
-	}
-
-	// b)
-	public static String tilStreng(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("tilStreng ikke implementert");
 		
-	}
+String print = "";
+		
+		for (int i = 0; i<tabell.length; i++) { 
+			
+		     print += "Tall " + i + ":  " + tabell[i] + "\n";
 
-	// c)
+	     } 
+		
+		System.out.println(print);
+} 
+	
+	// b) FERDIG //////////////////////////////////////////////////////////////////////////
+	public static String tilStreng(int[] tabell) {
+		
+String tabellVerdier = "";
+		
+		for (int i = 0; i<tabell.length; i++) { 
+			
+		     tabellVerdier += tabell[i];
+		     
+		  if (i<(tabell.length-1)) {   
+			  
+			  tabellVerdier += ",";
+			  
+		 } 
+ }		
+		
+		String print = "[" + tabellVerdier + "]";
+		
+		return print;
+}
+
+	// c) FERDIG /////////////////////////////////////////////////////////////////////////////
 	public static int summer(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("summer ikke implementert");
+		/// FOR LØKKE
+		int sumFor = 0;
+		
+		   for (int i = 0; i<tabell.length; i++) {
+			   
+			   sumFor += tabell[i];
+
+			}
+		       
+		       
+		 //// WHILE LØKKE
+	int sumWhile = 0;
+	int j = 0;
+			
+	while (j < tabell.length) {
+			   
+           sumWhile += tabell[j];
+		   j++;
+	 } 
+		            
+		       
+		///////// UTVIDET FOR LØKKE       
+		   	int sumForu = 0;	
+			
+			for(int k: tabell) {
+				
+				sumForu += k;
+				
+		     }
+	int returnValue = (sumFor + sumForu + sumWhile) - (sumFor + sumWhile);		       
+	return returnValue;
+		       		       
+}    
+
+	// d) FERDIG /////////////////////////////////////////////////////////////////////////
+	public static boolean finnesTall(int[] tabell, int tall) {	
+		
+		boolean funnet = false;	
+		
+		for (int i = 0; i<tabell.length; i++) {
+			
+			 if (tabell[i] == tall) {
+				funnet = true;
+			 }
+
+	     }	
+		return funnet;	
 	}
 
-	// d)
-	public static boolean finnesTall(int[] tabell, int tall) {
-
-		// TODO
-		throw new UnsupportedOperationException("finnesTall ikke implementert");
-	}
-
-	// e)
+	// e) FERDIG ////////////////////////////////////////////////////////////////////////////
 	public static int posisjonTall(int[] tabell, int tall) {
 
-		// TODO
-		throw new UnsupportedOperationException("posisjonTall ikke implementert");
-
-	}
-
-	// f)
-	public static int[] reverser(int[] tabell) {
-
-		// TODO
-		throw new UnsupportedOperationException("reverser ikke implementert");
+	    int posisjon = 0;	
+		boolean funnet = false;	
+		int i = 0;
+		
+		while ((funnet != true)&&(i<tabell.length)) {
+			
+		      if (tabell[i] == tall) {
+		    	  funnet = true;
+		      }
+			     if (funnet == true) {
+			    	 posisjon = i;
+			     } else posisjon = -1;
+			i++;
+	     }	
+		
+		return posisjon; 
 		
 	}
 
-	// g)
+	// f) FERDIG ////////////////////////////////////////////////////////////////////////////
+	
+	public static int[] reverser(int[] tabell) {
+
+		 int[] tabellRevers = new int[tabell.length];
+		    int j = tabell.length-1;
+		    
+		    for (int i = 0; i<tabell.length; i++) {
+		    	
+		    	tabellRevers[j] = tabell[i];
+		    	j--;
+		    	  	
+		    }
+	return tabellRevers;    
+ }
+
+	// g) FERDIG ////////////////////////////////////////////////////////////////////////////////
 	public static boolean erSortert(int[] tabell) {
 
-		// TODO
-		throw new UnsupportedOperationException("erSortert ikke implementert");
-	}
+		 boolean resultat = true;
+		   int i = 0;
+		   
+		   while ((resultat != false)&&(i<tabell.length-1)) {
+			   
+			   if(tabell[i]>tabell[i+1]) {
+				   resultat = false; 
+			   }
+			   i++;		
+ }
+		   return resultat;
+}		   
 
-	// h)
+	// h) FERDIG /////////////////////////////////////////////////////////////////////////
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
-
-		// TODO
-		throw new UnsupportedOperationException("settSammen ikke implementert");
-	}
-}
+		
+	int tabellKomb[] = new int[tabell1.length + tabell2.length];
+	int j = 0;
+		    
+		    for (int i = 0; i<tabellKomb.length; i+=2) {
+		    	
+		    	tabellKomb[j] = tabell1[j];
+		    	tabellKomb[j+tabell1.length] = tabell2[j];
+		    	
+		    	j++;
+             }
+	return tabellKomb;	    
+ }
+}	
